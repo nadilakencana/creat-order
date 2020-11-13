969b8663321200477059be24c253aea57c8cd172
nadilakencana <nadilakencana1107@gmail.com>
2020-11-13 11:17:00 +0700

    selesai

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+function Validasi(){
+  var detail = document.getElementById("detail").value;
+  var fromproduct = document.getElementById("formproduct").value;
+  
+
+  if (detail !="" && fromproduct !=""){
+    return true;
+    
+  }else{
+    alert("Form belum lengkap" );
+  }
+  document.getElementById("creat").value=true;
+}
+
+
 function Price()
   {
   var unit = document.getElementById("unit");
@@ -16,6 +31,26 @@ function Price()
 
 function TotalPrice (){
   var jumlah = document.getElementById("jumlah");
-  
+
+
+
   document.getElementById("total").value = (jumlah) * (document.getElementById("price").value) ;
+  document.getElementById("net").value = total;
 } 
+
+(function() {
+  $('form > input#form').keyup(function() {
+      var empty = false;
+      $('form > input').each(function() {
+          if ($(this).val() == '') {
+              empty = true;
+          }
+      });
+
+      if (empty) {
+          $('creat').attr('disabled', 'true');
+      } else {
+          $('creat').removeAttr('disabled');
+      }
+  });
+})()
